3fb863bbb5face2ffadc72da07aba80b5e2d9b1c
Paul Terry <paul.terry84@gmail.com>
2020-02-06 20:45:11 +0000

    Added form to the Register component

--- firebaseAuth/src/pages/Register/index.js
+++ firebaseAuth/src/pages/Register/index.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Register extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleOnChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
-      <div>Register Page</div>
+      <div>
+        <h1>ReactAuth</h1>
+        <div>
+          <p>Register</p>
+          <form>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleOnChange}
+                autoComplete="off" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleOnChange}
+                autoComplete="off" />
+            </div>
+            <div>
+              <button>Continue</button>
+            </div>
+            <p>Already got an account? <Link to='/signin'>Sign In</Link></p>
+          </form>
+        </div>
+      </div>
     );
   }
 }
