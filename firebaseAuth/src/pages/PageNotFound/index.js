dbed4d491073940f1b93a33e95041f088e7e2c28
Paul Terry <paul.terry84@gmail.com>
2020-02-06 19:52:35 +0000

    rearranged PageNotFound into functional component

--- firebaseAuth/src/pages/PageNotFound/index.js
+++ firebaseAuth/src/pages/PageNotFound/index.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { StyledPageNotFound } from './Styles';
 
-class PageNotFound extends Component {
-  render() {
-    return (
-      <StyledPageNotFound>
-        <p>404!</p>
-        <p>Doh! Page Doesn&apos;t Seem To Exist...</p>
-        <Link to="/">Go Home</Link>
-      </StyledPageNotFound>
-    );
-  }
-}
+const PageNotFound = () => {
+  return (
+    <StyledPageNotFound>
+      <p>404!</p>
+      <p>Doh! Page Doesn&apos;t Seem To Exist...</p>
+      <Link to="/">Go Home</Link>
+    </StyledPageNotFound>
+  );
+};
 
 export default PageNotFound;
