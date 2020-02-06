39f01a1917d0dd94112f22fd337732cb0e26f7ee
Paul Terry <paul.terry84@gmail.com>
2020-02-06 20:53:35 +0000

    Added code for the SignIn component

--- firebaseAuth/src/pages/SignIn/index.js
+++ firebaseAuth/src/pages/SignIn/index.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SignIn extends Component {
+  state = {
+    email: "",
+    password: ""
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
-      <div>SignIn Page</div>
+      <div>
+        <h1>ReactAuth</h1>
+        <div>
+          <p>Sign In</p>
+          <form>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleOnChange}
+                autoComplete="off" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleOnChange}
+                autoComplete="off" />
+            </div>
+            <div>
+              <button>Sign In</button>
+            </div>
+            <p>Need an account? <Link to='/register'>Register</Link></p>
+          </form>
+        </div>
+      </div>
     );
   }
 }
