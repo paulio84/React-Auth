5bb694a6155f6e44046015da4bffed6e04c2ecd2
Paul Terry <paul.terry84@gmail.com>
2020-02-07 12:42:41 +0000

    randomly select a lego man from randomuser.me for fun

--- firebaseAuth/src/pages/Profile/index.js
+++ firebaseAuth/src/pages/Profile/index.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 class Profile extends Component {
   render() {
+    const profilePic = Math.floor(Math.random() * 10);
+
     return (
       <div>
         <h1>Profile</h1>
         <div>
-          <img src="https://randomuser.me/api/portraits/med/lego/5.jpg" alt='profile pic' />
+          <img
+            src={`https://randomuser.me/api/portraits/med/lego/${profilePic}.jpg`}
+            alt='profile pic'
+          />
           <p>Name</p>
           <p>Occupation</p>
           <p>Message</p>
