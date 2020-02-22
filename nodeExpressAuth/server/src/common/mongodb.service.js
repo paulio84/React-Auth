aca5e51d0ffcf750bd22b6f192e3de24d3dd8f85
Paul Terry <paul.terry84@gmail.com>
2020-02-22 08:54:54 +0000

    Added MongoDB database connection

---
+++ nodeExpressAuth/server/src/common/mongodb.service.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const debug = require('debug')('mongodb');
+
+let count = 0;
+
+const options = {
+  poolSize: 10,
+  bufferMaxEntries: 0,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+const connectWithRetry = () => {
+  mongoose.connect(process.env.DATABASE_URL, options)
+    .then(() => {
+      debug('MongoDB is connected.');
+    })
+    .catch((err) => {
+      debug(`MongoDB connection unsuccessful, retry: ${count += 1}: ${err.message}`);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
+exports.mongoose = mongoose;
