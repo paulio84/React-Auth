5be0c547cb0abc25a2512391f6c77d326c4d899e
Paul Terry <paul.terry84@gmail.com>
2020-02-22 08:54:19 +0000

    Added error handling

---
+++ nodeExpressAuth/server/src/common/middlewares/errorHandling.js
@@ -0,0 +1,24 @@
+const notFound = ((req, res, next) => {
+  const err = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(err);
+});
+
+// eslint-disable-next-line no-unused-vars
+const errorHandler = ((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+
+  const returnObject = {
+    status: res.statusCode,
+    message: err.message
+  };
+  if (process.env.NODE_ENV !== 'production') returnObject.stack = err.stack;
+
+  res.json(returnObject);
+});
+
+module.exports = {
+  notFound,
+  errorHandler
+};
--- nodeExpressAuth/server/src/index.js
+++ nodeExpressAuth/server/src/index.js
@@ -5,12 +5,18 @@ const debug = require('debug')('app');
 
 require('dotenv').config();
 
+const { notFound, errorHandler } = require('./common/middlewares/errorHandling');
+
 const app = express();
 
 // load middlewares - morgan (for logging) and helmet (for setting various HTTP headers)
 app.use(morgan('common'));
 app.use(helmet());
 app.use(express.json());
+
+// handle errors
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 1234;
 app.listen(port, () => debug(`Listening at: http://localhost:${port}/`));
