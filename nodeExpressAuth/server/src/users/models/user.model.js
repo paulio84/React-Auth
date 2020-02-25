c1fff6975c701f5f2e86208bd3a6c65f7a1b6c45
Paul Terry <paul.terry84@gmail.com>
2020-02-25 19:50:50 +0000

    updated validation in the user model

--- nodeExpressAuth/server/src/users/models/user.model.js
+++ nodeExpressAuth/server/src/users/models/user.model.js
@@ -4,11 +4,16 @@ const { mongoose } = require('../../common/mongodb.service');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  username: { type: String, required: 'Username is required.', unique: true },
+  email: {
+    type: String,
+    required: 'Email is required.',
+    unique: true,
+    match: [/\S+@\S+\.\S+/, '"{VALUE}" is invalid.']
+  },
+  password: { type: String, required: 'Password is required.' }
 });
-userSchema.plugin(uniqueValidator, { message: '{PATH} "{VALUE}" is already taken.' });
+userSchema.plugin(uniqueValidator, { message: '"{VALUE}" is already taken.' });
 
 const User = mongoose.model('Users', userSchema);
 
