6854800e2be66c911445103454a99ebb2dcbb07c
Paul Terry <paul.terry84@gmail.com>
2020-02-25 19:51:12 +0000

    created JWT token when user registers

--- nodeExpressAuth/server/src/auth/controllers/auth.controller.js
+++ nodeExpressAuth/server/src/auth/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
 const userModel = require('../../users/models/user.model');
 const { VALIDATION_ERROR } = require('../../common/constants');
 
@@ -24,13 +26,26 @@ exports.Register = async (req, res, next) => {
       password: passwordHash
     };
     const createdUser = await userModel.CreateUser(user);
-    console.log(createdUser);
+
+    // create JWT token
+    const payload = { sub: createdUser.email, jti: uuid.v4() };
+    const secret = process.env.JWT_SECRET;
+    const options = {
+      issuer: process.env.JWT_ISSUER,
+      audience: process.env.JWT_AUDIENCE,
+      expiresIn: '2d'
+    };
+
+    const token = jwt.sign(payload, secret, options);
+    return res.status(201).json({
+      token,
+      expiredIn: options.expiresIn,
+      uid: createdUser.id
+    });
   } catch (err) {
     res.status(500);
     if (err.name === VALIDATION_ERROR) res.status(400);
 
     return next(err);
   }
-
-  return next();
 };
