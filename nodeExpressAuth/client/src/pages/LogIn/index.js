250a9136ac4f07547ee5c150ad62c769cb01063c
Paul Terry <paul.terry84@gmail.com>
2020-03-03 20:38:31 +0000

    Added LogIn form.
    Added LogIn form and setup useState to capture the user input.

--- nodeExpressAuth/client/src/pages/LogIn/index.js
+++ nodeExpressAuth/client/src/pages/LogIn/index.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 const LogIn = () => {
+  const [state, setState] = useState({});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  const handleInputChange = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
-    <h1>Log In</h1>
+    <Fragment>
+      <h1>Log In</h1>
+      {/* Can this be made more generic Log In and Register are essentially the same form */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            required
+            autoComplete='email'
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            required
+            autoComplete='current-password'
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button>Log In</button>
+        </div>
+      </form>
+    </Fragment>
   );
 };
 
